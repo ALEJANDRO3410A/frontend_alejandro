4925e38b3e24c25af4cb654952a0f8a341862e91
ALEJANDRO3410A <maa.ortiz@yavirac.edu.ec>
2024-11-26 16:47:29 -0500

    diapositiva 16

--- src/app/admin/inventario/components/categoria/categoria.component.ts
+++ src/app/admin/inventario/components/categoria/categoria.component.ts
@@ -1,13 +1,13 @@
-import { Component, inject, OnInit } from '@angular/core';
+import { Component, OnInit, inject } from '@angular/core';
 import { CategoriaService } from '../../services/categoria.service';
-import { FormControl, FormGroup } from '@angular/forms';
-import { error } from 'console';
+import { FormGroup, FormControl } from '@angular/forms';
 
+import Swal from 'sweetalert2';
 
 interface Categoria {
-  id?: number,
-  nombre: string;
-  detalle?: string
+  id: number,
+  nombre: string, // Cambié de 'nombreCategoria' a 'nombre'
+  detalle: string
 }
 
 @Component({
@@ -15,20 +15,20 @@ interface Categoria {
   templateUrl: './categoria.component.html',
   styleUrl: './categoria.component.scss'
 })
-
 export class CategoriaComponent implements OnInit {
 
-  private categoriaService = inject(CategoriaService)
+  private categoriaService = inject(CategoriaService);
 
-  categorias: Categoria[] = []
-  visible: boolean = false;
+  categorias: Categoria[] = [];
+  dialog_visible: boolean = false;
+  categoria_id: number = -1;
   categoriaForm = new FormGroup({
-    nombre: new FormControl(''),
+    nombre: new FormControl(''), // Cambié de 'nombreCategoria' a 'nombre'
     detalle: new FormControl('')
   });
 
   ngOnInit(): void {
-    this.getCategorias()
+    this.getCategorias();
   }
 
   getCategorias() {
@@ -39,22 +39,88 @@ export class CategoriaComponent implements OnInit {
       (error: any) => {
         console.log(error);
       }
-    )
+    );
   }
 
   mostrarDialog() {
-    this.visible = true
+    this.dialog_visible = true;
   }
 
   guardarCategoria() {
-    this.categoriaService.funGuardar(this.categoriaForm.value).subscribe(
-      (res: any) => {
-        this.visible = false;
-        this.getCategorias();
-      },
-      (error: any) => {
-        console.log(error);
+    if (!this.categoriaForm.valid) {
+      this.alerta("CAMPOS VACÍOS", "Por favor, completa todos los campos requeridos.", "error");
+      return;
+    }
+
+    if (this.categoria_id > 0) {
+      this.categoriaService.funModificar(this.categoria_id, this.categoriaForm.value).subscribe(
+        (res: any) => {
+          this.dialog_visible = false;
+          this.getCategorias();
+          this.categoria_id = -1;
+          this.alerta("ACTUALIZADO", "La categoría se modificó con éxito!", "success");
+        },
+        (error: any) => {
+          this.alerta("ERROR AL ACTUALIZAR", "Verifica los datos!", "error");
+        }
+      );
+    } else {
+      this.categoriaService.funGuardar(this.categoriaForm.value).subscribe(
+        (res: any) => {
+          this.dialog_visible = false;
+          this.getCategorias();
+          this.alerta("REGISTRADO", "La categoría se creó con éxito!", "success");
+        },
+        (error: any) => {
+          this.alerta("ERROR AL REGISTRAR", "Verifica los datos!", "error");
+        }
+      );
+    }
+    this.categoriaForm.reset();
+  }
+
+  editarCategoria(cat: Categoria) {
+    this.dialog_visible = true;
+    this.categoria_id = cat.id;
+    this.categoriaForm.setValue({
+      nombre: cat.nombre, // Cambié de 'nombreCategoria' a 'nombre'
+      detalle: cat.detalle,
+    });
+  }
+
+  eliminarCategoria(cat: Categoria) {
+    Swal.fire({
+      title: "¿Está seguro de eliminar la categoría?",
+      text: "Una vez eliminado no se podrá recuperar!",
+      icon: "warning",
+      showCancelButton: true,
+      confirmButtonColor: "#3085d6",
+      cancelButtonColor: "#d33",
+      confirmButtonText: "Si, eliminar!"
+    }).then((result) => {
+      if (result.isConfirmed) {
+        this.categoriaService.funEliminar(cat.id).subscribe(
+          (res: any) => {
+            this.alerta("ELIMINADO!", "Categoría eliminada.", "success");
+            this.getCategorias();
+            this.categoria_id = -1;
+          },
+          (error: any) => {
+            this.alerta("ERROR!", "Error al intentar eliminar.", "error");
+          }
+        );
+        this.categoriaForm.reset();
       }
-    )
+    });
+  }
+
+  alerta(title: string, text: string, icon: 'success' | 'error' | 'info' | 'question') {
+    Swal.fire({
+      title,
+      text,
+      icon,
+      showConfirmButton: false,
+      timer: 3000
+    });
   }
 }
